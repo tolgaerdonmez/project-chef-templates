5892bb14fb467979a79bdcab4109d1283b23a7c8
Tolga Erdonmez <tolgaerdonmez@gmail.com>
2020-06-23 22:48:18 +0300

    fix execSync encoding

--- frontend/nextjs-typescript/init/index.ts
+++ frontend/nextjs-typescript/init/index.ts
@@ -3,7 +3,9 @@ import { InitFunctionArgs } from "../../../../types/templates";
 import { infoMessage, successMessage } from "../../../../utils/messages";
 
 export default function initNextApp({ cwd }: InitFunctionArgs) {
-  const stdout = execSync(`yarn create next-app ${cwd} -e with-typescript`);
+  const stdout = execSync(`yarn create next-app ${cwd} -e with-typescript`, {
+    encoding: "utf-8",
+  });
   console.log(infoMessage(stdout));
   console.log(successMessage("Nextjs initialized successfully"));
 }
