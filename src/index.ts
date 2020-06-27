1ff1eb6700b5cdc4db969d797ad9de091eaf8325
Tolga Erdonmez <tolgaerdonmez@gmail.com>
2020-06-28 00:03:26 +0300

    checking local version function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
 import { join } from "path";
+import fetch from "isomorphic-unfetch";
 
 const currentPath = join(__dirname, "..");
 
 export default currentPath;
+
+const getOnlineVersion = async () => {
+  const endpoint = "https://api.npms.io/v2/package/project-chef-templates";
+  const data = await fetch(endpoint).then((data) => data.json());
+  const version = data.collected.metadata.version;
+  return version;
+};
+
+export const checkLocalVersion = async () => {
+  try {
+    const onlineVersion = await getOnlineVersion();
+    const { default: packageData } = await import(join("..", "package.json"));
+    const localVersion = packageData.version;
+
+    if (onlineVersion !== localVersion) {
+      return 1;
+    }
+
+    return 0;
+  } catch {
+    return -1;
+  }
+};
